ab0aa5ece816f4605038d5d42f14169979109229
Jegatharans <tharanjega@gmail.com>
2017-11-12 13:32:41 +0530

    Coding added

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
 
 public static void main(String[] args) {
-	System.out.println("Hello ATI");
+	int a= 10;
+	int b= 15;
+	int c=  a+b;
+
+	System.out.println("Total is"+ c);
 }
 }
